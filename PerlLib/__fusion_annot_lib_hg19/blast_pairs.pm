ebbde3c0716f50f3eaf1f2fbda6d8fee25df2904
Brian Haas <brianjohnhaas@gmail.com>
2015-11-12 16:08:55 -0500

    initial build process is complete

---
+++ PerlLib/__fusion_annot_lib_hg19/blast_pairs.pm
@@ -0,0 +1,30 @@
+package blast_pairs;
+
+use strict;
+use warnings;
+use Carp;
+
+sub load_data {
+    my ($annotations_href, $blast_pairs_gz_filename) = @_;
+    
+    print STDERR "-parsing blast pairs: $blast_pairs_gz_filename";
+    
+    open (my $fh, "gunzip -c $blast_pairs_gz_filename | ") or confess "Error, cannot read file via: gunzip -c $blast_pairs_gz_filename";
+    while (<$fh>) {
+        chomp;
+        my ($geneA, $geneB, $per_id, $Evalue) = split(/\s+/);
+        if ($geneA eq $geneB) { next; }
+        my $token = "{$geneA|$geneB|pID:$per_id|E:$Evalue}";
+        $annotations_href->{"$geneA--$geneB"}->{"BLASTMATCH:$token"} = 1;
+        $annotations_href->{"$geneB--$geneA"}->{"BLASTMATCH:$token"} = 1;
+    }
+    close $fh;
+    
+    return;
+}
+
+
+1; #EOM
+
+
+    
