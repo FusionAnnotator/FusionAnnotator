80dc92e9d83d078dadac8ee6a26bb5c588bb5786
Brian Haas <brianjohnhaas@gmail.com>
2017-06-30 11:43:43 -0400

    generalized approach to annotating fusions

--- testing/runMe.sh
+++ testing/runMe.sh
@@ -1,1 +1,6 @@
-../FusionAnnotator --fusion_annot_lib ../Hg19_CTAT_fusion_annotator_lib --annotate test.fusions
+
+if [ -z $CTAT_GENOME_LIB ]; then
+    echo Error, set env var CTAT_GENOME_LIB to the CTAT genome lib directory before running test.
+fi
+
+../FusionAnnotator --genome_lib_dir $CTAT_GENOME_LIB --annotate test.fusions
