1ffb9760658ed2c6540a54e1ac21c29b3cb24fd6
Brian Haas <brianjohnhaas@gmail.com>
2018-04-05 11:34:51 -0400

    allow to read from stdin

--- testing/runMe.sh
+++ testing/runMe.sh
@@ -3,4 +3,16 @@ if [ -z $CTAT_GENOME_LIB ]; then
     echo Error, set env var CTAT_GENOME_LIB to the CTAT genome lib directory before running test.
 fi
 
+echo "######################"
+echo "## Simple annotations"
+echo "#####################"
+
 ../FusionAnnotator --genome_lib_dir $CTAT_GENOME_LIB --annotate test.fusions
+
+echo "######################"
+echo "## Complex annotations"
+echo "#####################"
+
+../FusionAnnotator --genome_lib_dir $CTAT_GENOME_LIB --annotate test.fusions --full
+
+
